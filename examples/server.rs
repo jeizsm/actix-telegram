1fe5d7985acc006787d885bb9dd90a48135b78f0
Marat Safin <jeizsm@gmail.com>
2019-01-12 17:25:41 +0300

    example with state

--- examples/server.rs
+++ examples/server.rs
@@ -7,10 +7,11 @@ extern crate serde_json;
 
 use actix_telegram::actors::{telegram_server::*, TelegramApi};
 use actix_telegram::application::UpdateHandler;
-use actix_telegram::types::update::Update;
-use actix_telegram::App;
+use actix_telegram::types::update::{Update, UpdateKind};
 use actix_web::actix::{Actor, Addr, System};
 use std::env;
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
 
 fn main() {
     env_logger::init();
@@ -21,17 +22,30 @@ fn main() {
     let cert = Cert::new(env::var("CERTIFICATE_PEM").unwrap());
     let host = env::var("WEBHOOK_HOST").unwrap();
     let cert_and_key = CertAndKey::new(cert, key);
-    let _server = TelegramServer::new("127.0.0.1:59080".to_owned(), token, host, || Test)
-        .set_certificate_and_key(cert_and_key, true)
-        .start();
+    let arc = Arc::new(Mutex::new(HashMap::new()));
+    let _server = TelegramServer::new("127.0.0.1:59080".to_owned(), token, host, move || {
+        let clone = arc.clone();
+        Test { state: clone }
+    })
+    .set_workers(4)
+    .set_certificate_and_key(cert_and_key, true)
+    .start();
     sys.run();
 }
 
-struct Test;
+struct Test {
+    state: Arc<Mutex<HashMap<i64, ()>>>,
+}
 
 impl UpdateHandler for Test {
     fn handle(&self, update: Update, _: &Addr<TelegramApi>) -> Result<(), Update> {
-        println!("{:?}", update);
+        if let UpdateKind::Message(message) = update.kind() {
+            if let Some(user) = message.from() {
+                let mut hash_map = self.state.lock().unwrap();
+                hash_map.insert(*user.id(), ());
+            }
+        }
+        println!("{:?}", self.state);
         Ok(())
     }
 }
